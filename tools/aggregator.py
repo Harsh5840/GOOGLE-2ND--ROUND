da54c5da65d3554fad1774189b32b016e27a9afa
biscuitrescue <karttikeyasinha11@gmail.com>
2025-07-22 19:35:06 +0530

    removed tools/agglo

--- tools/aggregator.py
+++
@@ -1,8 +0,0 @@
-import os
-from ..agents import reddit_agent
-
-api_key = os.environ.get("GOOGLE_API_KEY")
-
-def reddit_wrapper(wrap: bool, name: str, id: str, secret: str, user_agent: str):
-    if wrap:
-        return reddit_agent.get_reddit_posts(name, id, secret, user_agent)
